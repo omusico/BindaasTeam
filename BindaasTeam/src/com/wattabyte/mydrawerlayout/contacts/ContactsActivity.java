b6caa51abb1cce3724bb26185b315a12f17e32fd
Vinayakam D <vinayagam.d@wattabyte.com>
2015-01-13 13:36:22 +0530

    Committ

---
+++ BindaasTeam/src/com/wattabyte/mydrawerlayout/contacts/ContactsActivity.java
@@ -0,0 +1,66 @@
+package com.wattabyte.mydrawerlayout.contacts;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import com.wattabyte.bindaasteam.R;
+
+
+
+public class ContactsActivity extends ActionBarActivity implements OnItemClickListener{
+	
+	private ArrayList<ContactUtil> listContacts;
+    private ListView lvContacts;
+    ConatctsAdapter adapterContacts;
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_contact); 
+    listContacts = new ContactFetcher(this).fetchAll();
+    lvContacts = (ListView) findViewById(android.R.id.list);
+     adapterContacts = new ConatctsAdapter(this, listContacts);
+    lvContacts.setAdapter(adapterContacts);
+    lvContacts.setOnItemClickListener(this);
+}
+
+
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+}
+
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+    if (id == R.id.action_settings) {
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+}
+
+
+@Override
+public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	
+	
+	
+}
+
+
+
+
+}
